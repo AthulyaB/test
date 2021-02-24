3c1a3da65962ca858ea5c8f1a116c3adab1e8c68
AthulyaB <78555762+AthulyaB@users.noreply.github.com>
2021-02-24 13:18:40 +0530

    Update newreg.php

--- application/views/newreg.php
+++ application/views/newreg.php
@@ -35,35 +35,35 @@
 			<tr>
 				<td>
 			FirstName:</td>
-			<td><input type="text"name="fname"placeholder="Enter the First Name">
+			<td><input type="text"name="fname"placeholder="Enter the First Name" required maxlength="25" pattern="[a-zA-Z]+">
 				</td>
 			</tr>
 			<br>
 			<tr>
 				<td>
 			LastName:</td>
-			<td><input type="text"name="lname"placeholder="Enter the Second Name">
+			<td><input type="text"name="lname"placeholder="Enter the Second Name" required maxlength="25" pattern="[a-zA-Z]+">
 				</td>
 			</tr>
 			<br>
 			<tr>
 				<td>
 			UserName:</td>
-			<td><input type="text"name="uname"placeholder="User Name">
+			<td><input type="text"name="uname"placeholder="User Name" required maxlength="25"pattern="[a-zA-Z0-9]+">
 				</td>
 			</tr>
 			<br>
 			<tr>
 				<td>
 			Password:</td>
-			<td><input type="text"name="pswd"placeholder="Password">
+			<td><input type="text"name="pswd"placeholder="Password" required  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter and atleast 8 or more characters"><br></td></tr>
 				</td>
 			</tr>
 			<br>
 			<tr>
 				<td>
 			Mobile:</td>
-			<td><input type="text"name="mobile"placeholder="Mobileno">
+			<td><input type="text"name="mobile"placeholder="Mobileno" required pattern="[7-9]{1}[0-9]{9}"  ><br>
 				</td>
 			</tr>
 			<br>
@@ -73,7 +73,7 @@
 			
 			<tr>
 				<td>
-			Email Id:</td><td><input type="Email"name="email" placeholder="email">
+			Email Id:</td><td><input type="Email"name="email" placeholder="email" required>
 				</td>
 			</tr>
 			<br>
